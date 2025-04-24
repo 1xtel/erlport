5f6213093342f6042baba16db293fc02cd6f0bde
onex-amey <amey@onextel.tech>
2025-04-24 18:31:16 +0530

    changed code to work with python 3.12.x

--- priv/python3/erlport/erlang.py
+++ priv/python3/erlport/erlang.py
@@ -25,7 +25,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from inspect import getargspec
+# from inspect import getargspec
+from inspect import getfullargspec as getargspec
 import sys
 from sys import exc_info
 from traceback import extract_tb
